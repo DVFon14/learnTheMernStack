8855192e320e6728dff8ff9ac9ab072839eae1a8
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-21 15:58:52 -0700

    Video2, time 10:35, register user

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ connectDB()
 
 const app = express()
 
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes')) //'/api/goals' is for the URL and './routes/goalRoutes' tells our code to look into our VS folders
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Server started at ${port}`))
