bf9140b967fb93556f0a145a47acf768f5c78d8e
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-21 12:05:44 -0700

    minute 36:44 in video

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 //get goals 
 //route get/api/goals
 //private access
-const getGoals = (req,res) =>{
+const getGoals = asyncHandler((req,res) =>{
     res.status(200).json({message:'Get goals'})
-}
+})
 
 //set goals 
 //route POST/api/goals
 //private access
-const setGoal = (req,res) =>{
+const setGoal = asyncHandler((req,res) =>{
     if(!req.body.text){
         res.status(400)
         throw new Error(`Please add a txt field`)
     }
     res.status(200).json({message:'Set goals'})
-}
+})
 
 //update goals 
 //route put/api/goals/:id
 //private access
-const updateGoal = (req,res) =>{
+const updateGoal = asyncHandler((req,res) =>{
     res.status(200).json({message:`update goal ${req.params.id}`})
-}
+})
 
 //delete goals 
 //route delete/api/goals/:id
 //private access
-const deleteGoal = (req,res) =>{
+const deleteGoal = asyncHandler((req,res) =>{
     res.status(200).json({message:`Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals, 
