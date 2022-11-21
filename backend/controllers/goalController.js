a4ef03b2136fb793c55326f395cb08bf66069a0b
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-21 15:37:22 -0700

    end of video 1

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -17,7 +17,7 @@ const getGoals = asyncHandler(async (req,res) => {
 const setGoal = asyncHandler(async (req,res) =>{
     if(!req.body.text){
         res.status(400)
-        throw new Error(`Please add a txt field`)
+        throw new Error(`Please add a text field`)
     }
 
     const goal = await Goal.create({
@@ -30,14 +30,35 @@ const setGoal = asyncHandler(async (req,res) =>{
 //route put/api/goals/:id
 //private access
 const updateGoal = asyncHandler(async (req,res) =>{
-    res.status(200).json({message:`update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id) //findById is a method. We get that id by using req.params
+  
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
+
 })
 
 //delete goals 
 //route delete/api/goals/:id
 //private access
 const deleteGoal = asyncHandler(async (req,res) =>{
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id) //findById is a method. We get that id by using req.params
+  
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
