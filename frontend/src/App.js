1df916cfd345702401883223a4201e9a67c20675
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-22 14:36:09 -0700

    Video 3, time 16:33, header and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 
 'react-router-dom'
+import Header from './pages/components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -8,8 +9,9 @@ import Register from './pages/Register'
 function App() {
   return (
     <>
-      <Router>
+       <Router>
         <div className='container'>
+          <Header />
           <Routes>
             <Route path='/' element= {<Dashboard />} />
             <Route path='/login' element= {<Login />} />
