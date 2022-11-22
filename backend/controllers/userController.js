17bdc17a1f2a9c34987ed1443bdf758e24fec357
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-21 21:22:42 -0700

    Video2, time 17:25

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,35 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 //register new user
 //POST /api/users
 //access is public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } =req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+    
     res.json({message: 'Register User'})
-}
+})
 
 //authenticate a user
 //POST /api/users/login
 //access is public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.json({message: 'Login User'})
-}
+})
 
 //get user data
 //GET /api/users
 //access is public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async(req, res) => {
     res.json({message: 'User data display'})
-}
+})
 
 module.exports = {
     registerUser,
